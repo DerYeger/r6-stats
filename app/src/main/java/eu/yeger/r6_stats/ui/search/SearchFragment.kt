77414d3c07f4a53f20774e0908f6278233eba6bd
Jan Müller <janmueller3698@gmail.com>
2020-01-31 17:32:30 +0100

    Added SearchFragment and -ViewModel

---
+++ app/src/main/java/eu/yeger/r6_stats/ui/search/SearchFragment.kt
@@ -0,0 +1,28 @@
+package eu.yeger.r6_stats.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import eu.yeger.r6_stats.databinding.SearchFragmentBinding
+
+class SearchFragment : Fragment() {
+
+    private lateinit var searchViewModel: SearchViewModel
+
+    private lateinit var binding: SearchFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        searchViewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
+        binding = SearchFragmentBinding.inflate(inflater)
+        binding.viewModel = searchViewModel
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+}
---
+++ app/src/main/java/eu/yeger/r6_stats/ui/search/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package eu.yeger.r6_stats.ui.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+
+}
