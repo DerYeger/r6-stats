725a5ca2dea6769818fe5ebf3646508a892be6b0
Jan Müller <janmueller3698@gmail.com>
2020-01-31 16:58:06 +0100

    Created project

---
+++ app/src/main/java/eu/yeger/r6_stats/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package eu.yeger.r6_stats.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/eu/yeger/r6_stats/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package eu.yeger.r6_stats.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
