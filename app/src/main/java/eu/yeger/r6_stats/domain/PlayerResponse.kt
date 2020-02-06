88f4c80137992da29df49bae071c60b383f8615b
Jan Müller <janmueller3698@gmail.com>
2020-02-06 13:46:37 +0100

    Expanded model

--- app/src/main/java/eu/yeger/r6_stats/domain/PlayerResponse.kt
+++ app/src/main/java/eu/yeger/r6_stats/domain/PlayerResponse.kt
@@ -31,4 +31,31 @@ data class PlayerResponse(
     val verified: Int,
     @Json(name = "kd")
     val kd: Int
-)
+) {
+    val rankedPlaytime = stats[0]
+    val rankedKills = stats[1]
+    val rankedDeaths = stats[2]
+    val rankedWins = stats[3]
+    val rankedLosses = stats[4]
+
+    val casualPlaytime = stats[5]
+    val casualKills = stats[6]
+    val casualDeaths = stats[7]
+    val casualWins = stats[8]
+    val casualLosses = stats[9]
+
+    val bombWins = stats[10]
+    val bombLosses = stats[11]
+
+    val secureWins = stats[12]
+    val secureLosses = stats[13]
+
+    val hostageWins = stats[14]
+    val hostageLosses = stats[15]
+
+    val totalBullets = stats[16]
+    val headshots = stats[17]
+    val melees = stats[18]
+    val revives = stats[19]
+    val suicides = stats[20]
+}
